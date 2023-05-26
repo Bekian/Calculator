32e6bbe2f9247f8ca8b58f19b6027fed56ee7efc
Bekian <47157992+Bekian@users.noreply.github.com>
2023-05-26 15:32:23 -0500

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+    int count2 = 0;
+    int total = 0;
+
+    public MainPage()
+	{
+		InitializeComponent();
+    }
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+        TotalingText();
+    }
+    private void OnCounterClicked2(object sender, EventArgs e)
+    {
+        count2++;
+
+        if (count2 == 1)
+            CounterBtn2.Text = $"Clicked {count2} time";
+        else
+            CounterBtn2.Text = $"Clicked {count2} times";
+
+        SemanticScreenReader.Announce(CounterBtn2.Text);
+        TotalingText();
+    }
+
+    private void TotalingText()
+    {
+        total = count + count2;
+
+        if (total == 1)
+            TotalText.Text = $"Clicked both buttons: {total} time";
+        else
+            TotalText.Text = $"Clicked both buttons: {total} times";
+
+        SemanticScreenReader.Announce(TotalText.Text);
+    }
+}
+
